f8be4ac128bd32184752c9784fcecdfe7a9cee96
jlassi1 <khawlajlassi1990@gmail.com>
2020-10-27 00:04:51 +0100

    first story push

---
+++ Khrafa/lib/main.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'sliders.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  final appTitle = 'Drawer Demo';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: appTitle,
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(title: appTitle),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(title)),
+      body: Center(child: Text('My Page!')),
+      bottomNavigationBar: BottomAppBar(
+          child: Container(
+        height: 90.0,
+        child: new Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.home),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.book),
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => Library()));
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.audiotrack),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      )),
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Drawer Header'),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              title: Text('Item 1'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Item 2'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ Khrafa/lib/sliders.dart
@@ -0,0 +1,102 @@
+import 'package:carousel_slider/carousel_options.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+import 'story.dart';
+
+final List<Map> imgList = [
+  {
+    "name": "خرافة الشمعة",
+    "image": 'assets/images/candle.jpg',
+  },
+  {
+    "name": "خرافة سبعة صباية في قصباية",
+    "image": 'assets/images/sabaya.jpg',
+    "routeName": "/image2Dashboard"
+  },
+  {
+    "name": "HOME",
+    "image": 'assets/images/home.jpg',
+    "routeName": "/simDashboard"
+  },
+{
+  "name": "امي سيسي",
+  "image": 'assets/images/omsisissi.jpg',
+}
+  ];
+
+final List<Widget> imageSliders = imgList
+    .map((item) => Container(
+          child: Container(
+            margin: EdgeInsets.all(5.0),
+            child: ClipRRect(
+                borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                child: Stack(
+                  children: <Widget>[
+                    Image.asset(item["image"],
+                        fit: BoxFit.cover, width: 1000.0, height: 2000.0),
+                    Positioned(
+                      bottom: 0.0,
+                      left: 0.0,
+                      right: 0.0,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [
+                              Color.fromARGB(200, 0, 0, 0),
+                              Color.fromARGB(0, 0, 0, 0)
+                            ],
+                            begin: Alignment.bottomCenter,
+                            end: Alignment.topCenter,
+                          ),
+                        ),
+                        padding: EdgeInsets.symmetric(
+                            vertical: 50.0, horizontal: 80.0),
+                        child: Text(
+                          ' ${item["name"]}',
+                          style: TextStyle(
+                            backgroundColor: Colors.white10,
+                            color: Colors.white,
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                )),
+          ),
+        ))
+    .toList();
+
+class Library extends StatelessWidget {
+  CarouselController buttonCarouselController = CarouselController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('pick up your story')),
+      body: 
+      Container(
+        child: Column(
+        children: <Widget>[
+              
+          CarouselSlider(
+            items: imageSliders,
+            carouselController: buttonCarouselController,
+            options: CarouselOptions(
+              autoPlay: true,
+              aspectRatio: 0.8,
+              enlargeCenterPage: true,
+              reverse: true,
+              
+            ),
+          ),
+           RaisedButton(
+        onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => Story()));}
+          ),
+        ],
+      )),
+    );
+  }
+}

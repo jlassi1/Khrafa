bc1b3b5060546e6037a46af6e5bc010d8f24bd02
jaycer95 <jacer.dabbabi95@gmail.com>
2020-10-27 14:21:48 +0100

    lib

---
+++ Khrafa/lib/home.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
+import 'sliders.dart';
+import 'config.dart';
+import 'rateus.dart';
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text(
+        title,
+        textDirection: TextDirection.ltr,
+        textAlign: TextAlign.right,
+      )),
+      body: Center(child: Text('My Page!')),
+      bottomNavigationBar: BottomAppBar(
+          child: Container(
+        height: 50.0,
+        child: new Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.home),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    PageTransition(
+                        type: PageTransitionType.bottomToTop,
+                        child: MyHomePage()));
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.book),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    PageTransition(
+                        type: PageTransitionType.bottomToTop,
+                        child: ComplicatedImageDemo()));
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.music_note),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      )),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Settings'),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.palette),
+              title: Text('Theme'),
+              subtitle:
+                  Text('Change from light theme to dark theme and backwards'),
+              onTap: () {
+                currentTheme.switchTheme();
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.star),
+              title: Text('Rate Us'),
+              onTap: () async {
+                int stars = await showDialog(
+                    context: context, builder: (_) => RatingDialog());
+
+                if (stars == null) return;
+
+                print('Selected rate stars: $stars');
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.phone),
+              title: Text('Contact'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- Khrafa/lib/main.dart
+++ Khrafa/lib/main.dart
@@ -1,193 +1,36 @@
-import 'package:Khrafa/data.dart';
 import 'package:flutter/material.dart';
-import 'BottomNavigationBar.dart';
-import 'dart:math';
+import 'config.dart';
+import 'home.dart';
 
+void main() => runApp(Khrafa());
 
-void main() {
-  runApp(MyApp());
+class Khrafa extends StatefulWidget {
+  const Khrafa({Key key}) : super(key: key);
+
+  @override
+  _KhrafaState createState() => _KhrafaState();
 }
 
-class MyApp extends StatelessWidget {
-  static const String _title = 'Khrafa';
+class _KhrafaState extends State<Khrafa> {
+  final appTitle = 'خرافة';
+  @override
+  void initState() {
+    super.initState();
+    currentTheme.addListener(() {
+      print('Changes');
+      setState(() {});
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: appTitle,
       debugShowCheckedModeBanner: false,
-      title: _title,
-      home: Khrafa(),
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      themeMode: currentTheme.currentTheme(),
+      home: MyHomePage(title: appTitle),
     );
-  }
-}
-
-class Khrafa extends StatefulWidget {
-  @override
-  _Khrafa createState() => new _Khrafa();
-}
-
-var cardAspectRatio = 12.0 / 16.0;
-var widgetAspectRatio = cardAspectRatio * 1.2;
-
-class _Khrafa extends State<Khrafa> {
-  var currentPage = images.length - 1.0;
-
-  @override
-  Widget build(BuildContext context) {
-    PageController controller = PageController(initialPage: images.length - 1);
-    controller.addListener(() {
-      setState(() {
-        currentPage = controller.page;
-      });
-    });
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Khrafa'),
-        flexibleSpace: Image(
-          image: AssetImage('assets/images/2.jpg'),
-          fit: BoxFit.fill,
-        ),
-        backgroundColor: Colors.transparent,
-      
-      ),
-      bottomNavigationBar: BottomWidget(),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: const <Widget>[
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-              child: Text(
-                'Settings',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                ),
-              ),
-            ),
-            ListTile(
-              leading: Icon(Icons.nightlight_round),
-              title: Text('Theme'),
-            ),
-            ListTile(
-              leading: Icon(Icons.language),
-              title: Text('Languege'),
-            ),
-            ListTile(
-              leading: Icon(Icons.star_rate),
-              title: Text('Rate Us'),
-            ),
-            ListTile(
-              leading: Icon(Icons.contact_support),
-              title: Text('Contact'),
-            ),
-          ],
-        ),
-      ),
-      body:  Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/images/icon1.jpg"),
-            fit: BoxFit.fill,
-          ),
-        ),
-        child: null /* add child content here */,
-      ),
-    );
-  }
-}
-
-class CardScrollWidget extends StatelessWidget {
-  var currentPage;
-  var padding = 20.0;
-  var verticalInset = 20.0;
-
-  CardScrollWidget(this.currentPage);
-
-  @override
-  Widget build(BuildContext context) {
-    return new AspectRatio(
-        aspectRatio: widgetAspectRatio,
-        child: LayoutBuilder(builder: (context, contraints) {
-          var width = contraints.maxWidth;
-          var height = contraints.maxHeight;
-
-          var safeWidth = width - 2 * padding;
-          var safeHeight = height - 2 * padding;
-
-          var heightOfPrimaryCard = safeHeight;
-          var widthOfPrimaryCard = heightOfPrimaryCard * cardAspectRatio;
-
-          var primaryCardLeft = safeWidth - widthOfPrimaryCard;
-          var horizontalInset = primaryCardLeft / 2;
-
-          List<Widget> cardList = new List();
-
-          for (var i = 0; i < images.length; i++) {
-            var delta = i - currentPage;
-            bool isOnRight = delta > 0;
-
-            var start = padding +
-                max(
-                    primaryCardLeft -
-                        horizontalInset * -delta * (isOnRight ? 15 : 1),
-                    0.0);
-
-            var cardItem = Positioned.directional(
-              top: padding + verticalInset * max(-delta, 0.0),
-              bottom: padding + verticalInset * max(-delta, 0.0),
-              start: start,
-              textDirection: TextDirection.rtl,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(16.0),
-                child: Container(
-                  decoration: BoxDecoration(color: Colors.white, boxShadow: [
-                    BoxShadow(
-                        color: Colors.black12,
-                        offset: Offset(3.0, 6.0),
-                        blurRadius: 10.0)
-                  ]),
-                  child: AspectRatio(
-                    aspectRatio: cardAspectRatio,
-                    child: Stack(
-                      fit: StackFit.expand,
-                      children: <Widget>[
-                        Image.asset(images[i], fit: BoxFit.cover),
-                        Align(
-                          alignment: Alignment.bottomLeft,
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Padding(
-                                padding: EdgeInsets.symmetric(
-                                    horizontal: 16.0, vertical: 8.0),
-                                child: Text(title[i],
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 25.0,
-                                        fontFamily: "SF-Pro-Text-Regular")),
-                              ),
-                              SizedBox(
-                                height: 10.0,
-                              ),
-                              
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            );
-            cardList.add(cardItem);
-          }
-          return Stack(
-            children: cardList,
-          );
-        }));
   }
 }
